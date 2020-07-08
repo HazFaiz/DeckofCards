6ef99a5588bbbc0d2660516d0c26bb25a60f37d4
Desktop <haz.faizul@gmail.com>
2020-07-08 22:39:58 +0800

    fixed issue with more than 52 players

--- index.js
+++ index.js
@@ -48,20 +48,34 @@ class Deck {
         let noOfCards = this.deck.length
         // console.log(noOfCards)
         //Need to round down cardsperPlayer as some number of players can result in decimals and giving undefined
-        let cardsPerPlayer = Math.floor(noOfCards / players)
+        let cardsPerPlayer;
+        // let cardsPerPlayer = Math.floor(noOfCards / players)
+
+        //If the # of players is past 52, we force each player to have 1 card. Doing this allows us to add players even though no more cards are left. Assuming that is we dont have to equally distribute cards
+        players >= 52 ? cardsPerPlayer = 1 : cardsPerPlayer = Math.floor(noOfCards / players)
         // console.log(cardsPerPlayer)
 
         for (let i = 0; i < players; i++) {
             let hand = []
             let player = { Name: 'Player ' + (i + 1), Hand: hand }
-            for (let j = 0; hand.length < cardsPerPlayer; j++) {
-                hand.push(this.deck.pop())
-                // console.log(` ${hand[j].value}`)
+
+            //If statement here, >52, give no more cards
+            if (i >= 52) {
+                // hand.push(`No more cards`)
+                hand.push({ suit: `Nil`, value: `Nil` })
+                this.allPlayers.push(player)
+            } else {
+
+                for (let j = 0; hand.length < cardsPerPlayer; j++) {
+                    hand.push(this.deck.pop())
+                    // console.log(` ${hand[j].value}`)
+                }
+                this.allPlayers.push(player)
+                // console.log(hand.length)
+                // console.log(` ${hand[0].value} of ${hand[0].suit} `)
 
             }
-            this.allPlayers.push(player)
-            // console.log(hand.length)
-            // console.log(` ${hand[0].value} of ${hand[0].suit} `)
+
 
         }
         return this.allPlayers
@@ -90,8 +104,8 @@ deck.shuffle()
 // console.log(deck.shuffle())
 // deck.deal()
 // console.log(deck.deal())
-// console.log(deck.dealMultiple(52))
-deck.dealMultiple(52)
+// console.log(deck.dealMultiple(54))
+deck.dealMultiple(20)
 deck.showHands()
 
 
